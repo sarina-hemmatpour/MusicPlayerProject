1bb2440939da70b1cb618ca64127aa4aadf3561a
Sarina Hemmat pour <hemmatpour.sarina@gmail.com>
2022-09-13 18:50:02 +0430

    Ui completed

--- app/src/main/java/com/example/musicplayerproject/MainActivity.java
+++ app/src/main/java/com/example/musicplayerproject/MainActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Fresco.initialize(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
